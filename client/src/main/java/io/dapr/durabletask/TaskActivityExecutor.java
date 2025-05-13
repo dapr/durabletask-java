85e0cac3f02416ab52a25f08d0c9a3691cfc38f5
Javier Aliaga <javier@diagrid.io>
2025-05-13 10:30:44 +0200

    chore: Introduce new task execution key
    All Task will receive a task execution key that can be use for idempotency checks
    
    Signed-off-by: Javier Aliaga <javier@diagrid.io>

--- client/src/main/java/io/dapr/durabletask/TaskActivityExecutor.java
+++ client/src/main/java/io/dapr/durabletask/TaskActivityExecutor.java
@@ -19,7 +19,7 @@ final class TaskActivityExecutor {
         this.logger = logger;
     }
 
-    public String execute(String taskName, String input, int taskId) throws Throwable {
+    public String execute(String taskName, String input, String taskExecutionKey) throws Throwable {
         TaskActivityFactory factory = this.activityFactories.get(taskName);
         if (factory == null) {
             throw new IllegalStateException(
@@ -32,7 +32,7 @@ final class TaskActivityExecutor {
                     String.format("The task factory '%s' returned a null TaskActivity object.", taskName));
         }
 
-        TaskActivityContextImpl context = new TaskActivityContextImpl(taskName, input);
+        TaskActivityContextImpl context = new TaskActivityContextImpl(taskName, input, taskExecutionKey);
 
         // Unhandled exceptions are allowed to escape
         Object output = activity.run(context);
@@ -44,19 +44,27 @@ final class TaskActivityExecutor {
     }
 
     private class TaskActivityContextImpl implements TaskActivityContext {
+        private final String taskExecutionKey;
         private final String name;
         private final String rawInput;
+                
 
         private final DataConverter dataConverter = TaskActivityExecutor.this.dataConverter;
 
-        public TaskActivityContextImpl(String activityName, String rawInput) {
+        public TaskActivityContextImpl(String activityName, String rawInput, String taskExecutionKey) {
             this.name = activityName;
             this.rawInput = rawInput;
+            this.taskExecutionKey = taskExecutionKey;
         }
 
         @Override
         public String getName() {
             return this.name;
+        }
+
+        @Override
+        public String getTaskExecutionKey() {
+            return this.taskExecutionKey;
         }
 
         @Override
